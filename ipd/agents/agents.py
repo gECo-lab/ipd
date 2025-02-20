63dab3f2db8a1f3d866cf516acee3fa6d1bb0338
Sergio Rivero <sergiolmrivero@gmail.com>
2025-02-20 15:13:30 -0300

    Desenvolvendo a memória genérica do agente

--- ipd/agents/agents.py
+++ ipd/agents/agents.py
@@ -6,7 +6,8 @@ from .ipd_action_set import *
 
 
 class Player(DiscreteEventAgent):
-    """ A basic player in the Iterated Prisioners Dilemma """
+    """A basic player in the Iterated Prisioners Dilemma"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.my_payoff = 0
@@ -20,16 +21,16 @@ class Player(DiscreteEventAgent):
         self.mean_payoff = 0
 
     def step(self):
-        """ The agent selects a play from a strategy """
-#        self.select_game()
+        """The agent selects a play from a strategy"""
+        #        self.select_game()
         self.update_mean_payoff()
 
     def play(self):
-        """ The agent plays a strategy """
+        """The agent plays a strategy"""
         return self.my_play
 
     def game_payoff(self, other_name, other_play, other_payoff, my_payoff):
-        """ Get the game payoff """
+        """Get the game payoff"""
         self.my_payoff = my_payoff
         self.other_name = other_name
         self.other_play = other_play
@@ -39,71 +40,76 @@ class Player(DiscreteEventAgent):
         self.game.other_play = other_play
         self.game.other_payoff = other_payoff
         self.strategy.update_game(self.game)
-    
+
     def select_game(self, other_player):
-        """ The player selects it's game"""
+        """The player selects it's game"""
         self.my_play = self.strategy.select_game(other_player)
 
     def update_mean_payoff(self):
-        """ The player calculates it's average payoff """
-        self.mean_payoff = (self.mean_payoff + self.my_payoff)/2
+        """The player calculates it's average payoff"""
+        self.mean_payoff = (self.mean_payoff + self.my_payoff) / 2
 
-    
+
 class GoodPlayer(Player):
-    """ A player that always cooperate """
+    """A player that always cooperate"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.strategy = AlwaysCooperate()
 
 
 class BadPlayer(Player):
-    """ A player that always defect """
+    """A player that always defect"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.strategy = AlwaysDefect()
 
 
-
 class RandomPlayer(Player):
-    """ A player that randomly plays """
+    """A player that randomly plays"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.strategy = RandomPlay()
 
 
 class TitForTatPlayer(Player):
-    """ Tit for tat player """
+    """Tit for tat player"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.strategy = SimpleTitForTat()
 
 
 class TitForTatWithRecallPlayer(Player):
-    """ Tit for tat player """
+    """Tit for tat player"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.strategy = TitForTat()
 
+
 class RancorousPlayer(Player):
-    """ Rancorous player """
+    """Rancorous player"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.strategy = SimpleRancorous()
 
 
 class RancorousWithRecallPlayer(Player):
-    """ Rancorous player - Impl: Lucas 2023-10-25 """
+    """Rancorous player - Impl: Lucas 2023-10-25"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.strategy = Rancorous()
 
+
 class GenericStrategyPlayer(Player):
     """Implemnts a player with memory and generic strategies"""
+
     def __init__(self, simulation, model, agent_number, agent_def):
         super().__init__(simulation, model, agent_number, agent_def)
         self.strategy = Generic()
         self.memory = Memory()
-
-
-
-
