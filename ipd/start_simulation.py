87bee89b80cd7fd03773e673521409f03b8cd35c
Sergio Rivero <sergiolmrivero@gmail.com>
2025-04-29 16:51:21 -0300

    Changing app starting point

--- ipd/start_simulation.py
+++ ipd/start_simulation.py
@@ -12,8 +12,23 @@ def main():
         with open(activate_script) as f:
             exec(f.read(), {'__file__': activate_script})
 
+
+    # Read directories and filenames from config.json
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+        kernel_dir = config["paths"]["kernel"]
+        analisys_dir = config["paths"]["analisys"]
+        results_dir = config["paths"]["results"]
+        runs_dir = config["paths"]["runs"]
+        model_file = config["files"]["model"]
+        scenarios_file = config["files"]["scenarios"]
+        analisys_file = config["files"]["analisys"]
+        results_file = config["files"]["results"]
+
+
+
+
     # Remove CSV files in the 'runs' directory
-    runs_dir = os.path.join(os.getcwd(), "runs")
     if os.path.exists(runs_dir):
         for file in os.listdir(runs_dir):
             if file.endswith(".csv"):
@@ -22,16 +37,7 @@ def main():
     # Get current directory as app_dir
     app_dir = os.getcwd()
 
-    # Read kernel_dir from config.json
-    with open("config.json") as config_file:
-        config = json.load(config_file)
-        kernel_dir = config["paths"]["kernel"]
-        analisys_dir = config["paths"]["analisys"]
-        results_dir = config["paths"]["results"]
-        model_file = config["files"]["model"]
-        scenarios_file = config["files"]["scenarios"]
-        analisys_file = config["files"]["analisys"]
-
+   
     # Change directory to kernel_dir and run ecosimp.py
     os.chdir(kernel_dir)
     subprocess.run(["python3", "ecosimp.py", app_dir, model_file, scenarios_file], check=True)
@@ -52,9 +58,9 @@ def main():
     subprocess.run(["Rscript", "-e", r_command], check=True)
 
     # Open the resulting HTML file in the default web browser
-    results_file = os.path.join(app_dir, results_dir, "ipd.html")
+    results_path = os.path.join(app_dir, results_dir, results_file)
     if os.path.exists(results_file):
-        webbrowser.open(f"file://{results_file}")
+        webbrowser.open(f"file://{results_path}")
     else:
         print("Error: Results file not found.")
 
