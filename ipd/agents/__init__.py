c5739071993f7a071087940e03608da0271e5522
Sergio Rivero <sergiolmrivero@gmail.com>
2025-08-19 14:11:46 -0300

    commit init.py

--- ipd/agents/__init__.py
+++ ipd/agents/__init__.py
@@ -1,7 +1,8 @@
 from .agents import *
-__all__ = ["Player", "GoodPlayer", "BadPlayer", "RandomPlayer", 
-           "TitForTatPlayer", "TitForTatWithRecallPlayer", 
-           "RancorousPlayer", "HardTitForTatPlayer",
-           "GenericStrategyPlayer", "RancorousWithRecallPlayer",
-            "PeriodicPlayer", "SlowTitForTatPlayer", 
-            "TitFor2TatPlayer", "GradualPlayer"]
+__all__ = ["Player", "GoodPlayer", "BadPlayer", "RandomPlayer", "TitForTatPlayer", 
+           "TitForTatWithRecallPlayer", "RancorousPlayer", "PeriodicPlayer", 
+           "TitFor2TatPlayer", "GradualPlayer", "HardTitForTatPlayer", 
+           "SlowTitForTatPlayer", "PavlovPlayer", "ProberPlayer", 
+           "SoftMajorityPlayer", "HardMajorityPlayer", "MistrustPlayer", 
+           "MemPlayer", "ZDEqualizerPlayer", "ZDEqualizerPlayer"
+           ]
